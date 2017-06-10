aa96f50286331b5db5e518caaf06f166cef74a7e
li-zhijian <martin.li@mail.utoronto.ca>
2017-06-10 16:19:25 -0700

    add sliding sidenav

--- js/index.js
+++ js/index.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
 
-	$(".drop-button").click(function() {
-		$(".dropdown-content").toggle();
-	})
+	$(".openNav").click(function() {
+		// $(".sidenav").toggle();
+		$(".sidenav").css("width", "250px");
+		$("header").css("margin-left", "250px");
+		$(".main-content").css("margin-left", "250px");
+	});
+
+	// hide sidenav if it is visible
+	$(document).click(function(event) {
+		if (!$(event.target).closest(".openNav").length) {
+			if ($(".sidenav").is(":visible")) {
+				$(".sidenav").css("width", "0");
+				$("header").css("margin-left", "0");
+				$(".main-content").css("margin-left", "0");
+				// alert("hello");
+			}
+		}
+	});
+	
 })
