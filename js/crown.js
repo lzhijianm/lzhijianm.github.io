09984bb6b4a8d40902266601e01b7f3fbecf41d8
li-zhijian <martin.li@mail.utoronto.ca>
2017-06-23 16:09:43 -0700

    added modal for crown

--- js/crown.js
+++ js/crown.js
@@ -1,5 +1,15 @@
 $(document).ready(function() {
-
-
-	
+	// Get the image and insert it inside the modal 
+	// - use its "alt" text as a caption
+	if ($(".bool").css("float") == "none") {
+		$(".thumbnail-img").click(function() {
+			$("#myModal").css("display", "block");
+			$("#img01").attr("src", this.src);
+			$("#caption").html(this.alt);
+		});
+		// When the user clicks on <span> (x), close the modal
+		$("#myModal").click(function() {
+			$("#myModal").css("display", "none");
+		});
+	}
 })
