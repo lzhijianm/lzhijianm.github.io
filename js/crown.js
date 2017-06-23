adea6332af67147afa0a938de10b0fb642fda695
li-zhijian <martin.li@mail.utoronto.ca>
2017-06-23 16:26:49 -0700

    fixed small bugs for crown.js

--- js/crown.js
+++ js/crown.js
@@ -1,7 +1,25 @@
 $(document).ready(function() {
 	// Get the image and insert it inside the modal 
 	// - use its "alt" text as a caption
-	if ($(".bool").css("float") == "none") {
+	$(".openNav").click(function() {
+		// $(".sidenav").toggle();
+		$(".sidenav").show();
+		$("header").css("margin-left", "250px");
+		$(".crown-content").css("margin-left", "250px");
+	});
+
+	// hide sidenav if it is visible
+	$(document).click(function(event) {
+		if (!$(event.target).closest(".openNav").length) {
+			if ($(".sidenav").is(":visible")) {
+				$(".sidenav").hide();
+				$("header").css("margin-left", "0");
+				$(".crown-content").css("margin-left", "0");
+			}
+		}
+	});
+
+	if ($(".bool").css("float") == "none" ) {
 		$(".thumbnail-img").click(function() {
 			$("#myModal").css("display", "block");
 			$("#img01").attr("src", this.src);
